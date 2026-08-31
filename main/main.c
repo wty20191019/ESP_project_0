401bee98e3f44ae01c2dd047a528b0b9308b1d64
wty20191019 <906938998@qq.com>
2026-08-31 22:57:25 +0800

    添加 ESP-IDF 日志功能，替换 printf 输出

--- main/main.c
+++ main/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
 
 void app_main(void)
 {
-    printf("Hello world!\n");
+    
+    
+    
+    
+    while (1)
+    {
+        
+        //printf("Hello world!\n");
+        esp_log_write(ESP_LOG_INFO, "main", "Hello world!\n");
+        
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    }
 }
